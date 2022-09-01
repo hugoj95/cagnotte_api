72fdf75c12a7a0dc836e017e4c9c75fafcd6b6f6
hugo jadoul <hugo.jadoul@gmail.com>
2022-09-01 16:48:35 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { config } from './config/config';
+
+const app = express();
+const port = config.server.port; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello hugo!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `yo le serveur est lancé au http://localhost:${ port }` );
+} );
