84ceb86ea9303cec656cede08a0ea2a2c7745b00
hugo jadoul <hugo.jadoul@gmail.com>
2022-09-01 17:14:49 +0200

    anus

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const port = config.server.port; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello hugo!" );
+    res.send( "Hello hugo! anus" );
 } );
 
 // start the Express server
