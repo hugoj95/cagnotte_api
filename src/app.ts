24015207ed3aab0674d504d113a5b909e852d29c
hugo jadoul <hugo.jadoul@gmail.com>
2022-09-04 20:47:36 +0200

    non sleeping

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from './config/config';
 
 const app = express();
+app.use(express.json())
 const port = config.server.port; // default port to listen
 
 // define a route handler for the default home page
@@ -13,3 +14,8 @@ app.get( "/", ( req, res ) => {
 app.listen( port, () => {
     console.log( `yo le serveur est lancé au http://localhost:${ port }` );
 } );
+
+const http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000) pour empécher l'app de dormir
